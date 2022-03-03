18d608e334a268f51cce22fd9b3a1c0f71d2a92d
Kyle Farwell <m@kfarwell.org>
2022-03-03 05:22:19 -0600

    initial commit: GoldenLayout, Xterm.js demo

---
+++ README.md
@@ -0,0 +1,16 @@
+# GelatOS
+The Gelato "Operating System".
+
+## Build
+```
+git clone https://github.com/gelatolabs/gelatos
+cd gelatos
+npm install
+```
+
+## Contribute
+[Discord](https://discord.gg/6WEwGsRHvp)
+
+## License
+GelatOS is copyright (c) 2021-2022 Gelato Labs and is distributed under
+the ISC license. See LICENSE for details.
---
+++ js/main.js
@@ -0,0 +1,58 @@
+var config = {
+    content: [{
+        type: 'row',
+        isClosable: false
+    }]
+};
+var layout = new GoldenLayout(config, $('#layoutContainer'));
+
+layout.registerComponent('window', function(container, state) {
+    container.getElement().html(state.content);
+});
+
+layout.init();
+
+/* GelatoHello */
+var menuHello = $('<li>GelatoHello</li>');
+$('#menuContainer').append(menuHello);
+menuHello.click(function() {
+    layout.root.contentItems[0].addChild({
+        title: 'GelatoHello',
+        type: 'component',
+        componentName: 'window',
+        componentState: { content: '<p>Hello, world!</p>' }
+    });
+});
+
+/* GelatoTerm */
+var termNum = 0;
+var menuTerm = $('<li>GelatoTerm</li>');
+$('#menuContainer').append(menuTerm);
+menuTerm.click(function() {
+    layout.root.contentItems[0].addChild({
+        title: 'GelatoTerm',
+        type: 'component',
+        componentName: 'window',
+        componentState: {
+            content: '<div class="termContainer" id="termContainer-' + termNum + '"></div>'
+        }
+    });
+    createTerm($('#termContainer-' + termNum)[0]);
+});
+
+function createTerm(termContainer) {
+    var term = new Terminal({
+        fontFamily: 'Go-Mono, monospace',
+        theme: {
+            foreground: '#0f0'
+        }
+    });
+    term.open(termContainer);
+    term.focus();
+
+    var termFit = new FitAddon.FitAddon();
+    term.loadAddon(termFit);
+    termFit.fit(termContainer.clientWidth, termContainer.clientHeight);
+
+    termNum++;
+}
